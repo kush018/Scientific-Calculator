ae9103fd0322af80ed13613173220d22dd352643
Kushal Galrani <kushal.galrani@gmail.com>
2021-04-07 16:30:34 +0530

    updated build scripts

--- compile.bat
+++ compile.bat
@@ -2,6 +2,4 @@
 cd "C:\Users\kushal2\IdeaProjects\Scientific Calculator"
 javac -d ./ src/*.java
 jar mfcv "out/production/Scientific Calculator/META-INF/MANIFEST.MF" calc.jar *.class
-java -jar D:\launch4j\launch4j.jar create_exe_launch4j.xml
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" create_installer.iss
 pause
